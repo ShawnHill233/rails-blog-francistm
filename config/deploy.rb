a295c2017cd2970971bb58d5d6f74dd18bdf83c1
francis <francis.tm@gmail.com>
2013-12-12 17:03:00 +0800

    add capistrano deploy script

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+set :application, 'rails-blog'
+set :repo_url, 'git@github.com:francistm/rails-blog.git'
+
+set :scm, :git
+set :rails_env, "production"
+set :deploy_to, '/var/rails/francis_blog'
+
+set :rvm_type, :system
+set :rvm_ruby_version, "2.0.0-p353"
+
+namespace :symlink do
+  task :config do
+    on roles(:app), :in => :sequence, :wait => 5 do
+      execute "if [ ! -e '#{release_path}/config/database.yml' ]; then ln -s #{shared_path}/database.yml #{release_path}/config; fi"
+    end
+  end
+end
+
+namespace :deploy do
+  task :stop do
+    on roles(:app), :in => :sequence, :wait => 5 do
+      execute "service puma stop"
+    end
+  end
+
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # link puma state files from shared path
+      execute "if [ ! -e '#{shared_path}/puma' ]; then mkdir -p #{shared_path}/puma; fi"
+      execute "if [ ! -e '#{current_path}/tmp/puma' ]; then ln -s #{shared_path}/puma #{current_path}/tmp; fi"
+      execute "service puma start"
+    end
+  end
+
+  before "deploy:updated", "symlink:config"
+  before "deploy:symlink:release", "deploy:stop"
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,5 @@
+set :stage, :production
+
+role :app, %W(root@118.193.54.143)
+role :web, %W(root@118.193.54.143)
+role :db,  %W(root@118.193.54.143)
