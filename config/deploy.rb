6f16d562002cb068593e0a36e9a34f0d7cdcc72a
francis <francis.tm@gmail.com>
2013-12-20 14:05:06 +0800

    optimize deploy script
    will not stop the server after updated the code
    server will restart directly instead stop and start

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,21 +19,14 @@ namespace :symlink do
 end
 
 namespace :deploy do
-  task :stop do
-    on roles(:app), :in => :sequence, :wait => 5 do
-      execute "cd #{current_path} && ( bundle exec pumactl -P #{current_path}/tmp/puma/pid stop )"
-    end
-  end
-
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # link puma state files from shared path
       execute "if [ ! -e '#{shared_path}/puma' ]; then mkdir -p #{shared_path}/puma; fi"
       execute "if [ ! -e '#{current_path}/tmp/puma' ]; then ln -s #{shared_path}/puma #{current_path}/tmp; fi"
-      execute "cd #{current_path} && ( bundle exec puma -d -C #{current_path}/config/puma.rb 2>&1 >> #{current_path}/log/puma.log )"
+      execute "cd #{current_path} && ( bundle exec pumactl -P #{current_path}/tmp/puma/pid restart )"
     end
   end
 
   before "deploy:updated", "symlink:config"
-  before "deploy:symlink:release", "deploy:stop"
 end
